68161e96de77f132e7043d6ebe10ad58aef56f48
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2024-11-24 22:17:19 -0500

    static file path test

--- server.js
+++ server.js
@@ -21,3 +21,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/test', (req, res) => {
+    res.send('Test route is working!');
+});
